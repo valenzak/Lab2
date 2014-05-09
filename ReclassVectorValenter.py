d3c1a53b02c888952ca732a64ff5ad3fc996b434
Zachary Valenter <valenzak@uw.edu>
2014-05-09 01:44:58 -0700

    Final commit
    Final

--- ReclassVectorValenter.py
+++ ReclassVectorValenter.py
@@ -2,42 +2,39 @@
 import arcpy
 import os
 
-arcpy.env.workspace = "C://Data"
-
-
-###Make three lists which hold classification table parameters
-
+###Make/define three lists which hold classification table parameters
+##Lowbound list
 Llist = []
+##Highbound list
 Ulist = []
+##Values/Classifications list
 Vlist = []
 
+###Fill the lists using fields from the table
+##Define the searchcursor
 cursor = arcpy.da.SearchCursor(arcpy.GetParameterAsText(0), [arcpy.GetParameterAsText(1), arcpy.GetParameterAsText(2), arcpy.GetParameterAsText(3)])
 for row in cursor:
-    Llist.append(row[0])
-    Ulist.append(row[1])
-    Vlist.append(row[2])
+    Llist.append(row[0]) #Add lowbounds to lowbound list
+    Ulist.append(row[1]) #Add highbounds to highbound list
+    Vlist.append(row[2]) #Add values/classifications to list
+##End the cursor
 del cursor
 
-
-
-###Make a blank field
-##Define variables in AddField_management
 #Specify the existing table
 in_table = arcpy.GetParameterAsText(4)
 #Name the new field
 field_name = arcpy.GetParameterAsText(7)
-
+#New layer as an output
 out_class = arcpy.GetParameterAsText(6)
 
+##Create a new layer and populate it with the input layer
 arcpy.CopyFeatures_management(in_table, out_class)
 
-##Execute adding the "Double" field with the defined variables to the table
+##Execute adding a new "Double" field with the defined variables to the output table
 arcpy.AddField_management(out_class, field_name, "DOUBLE")
 
-
-###Populate the field
+###Populate the new field in the new layer
 ##Define Cursor 2
-##Messages cannot adequately describe the things I did next.  These loops are the result of countless trial and error hours.
 cursor2 = arcpy.da.UpdateCursor(out_class, [arcpy.GetParameterAsText(5), arcpy.GetParameterAsText(7)])
 for row2 in cursor2: 
         if row2[0] < Llist[0]:
